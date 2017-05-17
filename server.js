43b2ecf6cf77d248d4e949173e7e34aac2d5b3f1
Ewenblitz <ewen.lomer@ynov.com>
2017-05-17 16:20:52 +0200

    Server OK
    Server OK niveau multi a tester

--- server.js
+++ server.js
@@ -78,6 +78,87 @@ function broadcast(event, data) {
   });
 }
 
+var app = express(app);
+
+//On récupère les fichiers static du répertoire actuel
+app.use(express.static(__dirname));
+
+//nous gardon les données des clients
+var clients = {};
+
+//get EurecaServer class
+var EurecaServer = require('eureca.io').EurecaServer;
+
+//création d'une instance EurecaServer
+var eurecaServer = new EurecaServer({allow:['setId', 'spawnEnemy', 'kill', 'updateState']});
+
+//on attache eureca.io sur notre serveur http
+eurecaServer.attach(server);
+
+//eureca.io fournit des évenements qui permet de détecter la connexion/déconnexion d'un client
+
+//détection d'une connexion de client
+eurecaServer.onConnect(function (conn) {
+  console.log('Nouveau Client id=%s ', conn.id, conn.remoteAddress);
+
+  //la method getClient fourni un proxy qui nous permet d'appeller les fonction clientes
+  var remote = eurecaServer.getClient(conn.id);
+
+  //enregistre le client
+  clients[conn.id] = {id:conn.id, remote:remote}
+
+  //appel de la methode setId définie côté client
+  remote.setId(conn.id);
+});
+
+//détéction de la déconnexion d'un client
+eurecaServer.onDisconnect(function (conn) {
+  console.log('Client déconnecté ', conn.id);
+
+  var removeId = clients[conn.id].id;
+
+  delete clients[conn.id];
+
+  for (var c in clients)
+  {
+    var remote = clients[c].remote;
+
+    //ici on appel la methode kill() définie côté client
+    remote.kill(conn.id);
+  }
+});
+
+eurecaServer.exports.handshake = function()
+{
+  for (var c in clients)
+  {
+    var remote = clients[c].remote;
+    for (var cc in clients)
+    {
+      //envoie de la dernière position connue
+      var x = clients[cc].laststate ? clients[cc].laststate.x: 0;
+      var y = clients[cc].laststate ? clients[cc].laststate.y: 0;
+
+      remote.spawnEnemy(clients[cc].id, x, y);
+    }
+  }
+}
+
+//exposition du côté client
+eurecaServer.exports.handleKeys = function (keys) {
+  var conn = this.connection;
+  var updatedClient = clients[conn.id];
+
+  for (var c in clients)
+  {
+    var remote = clients[c].remote;
+    remote.updateState(updateClient.id, keys);
+
+    //on garde le dernier état connu et on l'envoie au nouveau client connecté
+    clients[c].laststate = keys;
+  }
+}
+
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
